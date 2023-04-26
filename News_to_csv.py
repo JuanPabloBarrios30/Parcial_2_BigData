aeb9fbe199d2de475e4a74d069013bb24e8ddba9
Ubuntu <ubuntu@ip-172-31-51-235.ec2.internal>
2023-04-26 03:11:46 +0000

    Pruebas unitarias

--- News_to_csv.py
+++ News_to_csv.py
@@ -10,26 +10,39 @@ fecha = date.today()
 s3 = boto3.client('s3')
 s3_resource = boto3.resource('s3')
 
+def get_link_et(links):
+    texto = ''
+    try:
+      link='"'+links["href"]+'"'
+      categoria='"'+link.split('/')[1]+'"'
+      titulo='"'+(links.text)+'"'
+      texto+=f'{titulo},{categoria},{link}\n'
+    except:
+        ...
+    return texto
+
+def get_link_ee(links):
+    texto = ''
+    try:
+      link=links["href"]
+      categoria=link.split('/')[1]
+      titulo=((link.replace('-',' ')).replace(categoria,'')).replace('/','')
+      texto+=f'{titulo},{categoria},{link}\n'
+    except:
+      ...
+    return texto
+    
+
 def extraerTiempo(file):
     contenido = s3.get_object(Bucket=S3_BUCKET, Key=file)["Body"].read() 
     soup = BeautifulSoup(contenido, 'html.parser')
-
     texto='titulo,categoria,link\n'
     
     for articles in soup.find_all('article'):
         for links in articles.find_all('a',class_='title page-link'):
-            try:
-              link='"'+links["href"]+'"'
-              #print(link)
-              categoria='"'+link.split('/')[1]+'"'
-              #print(categoria)
-              titulo='"'+(links.text)+'"'
-              texto+=f'{titulo},{categoria},{link}\n'
-            except:
-                ...
-             
-
-    url="headlines/final/periodico=ElTiempo/year="+str(fecha.year)+"/month="+str(fecha.strftime('%m'))+"/elTiempo"
+            texto += get_link_et(links)
+            
+    url="headlines/final/periodico=ElTiempo/year="+str(fecha.year)+"/month="+str(fecha.strftime('%m'))+"/day="+str(fecha.strftime('%d'))+"/elTiempo"
     s3_resource.Object(S3_BUCKET, url+'{}.csv'.format(fecha.strftime('%Y-%m-%d'))).put(Body=texto)
  
 def extraerEspectador(file):
@@ -39,14 +52,9 @@ def extraerEspectador(file):
 
     for articles in soup.find_all('h2',class_='Card-Title Title Title'):
       for links in articles.find_all('a'):
-        try:
-          link=links["href"]
-          categoria=link.split('/')[1]
-          titulo=((link.replace('-',' ')).replace(categoria,'')).replace('/','')
-          texto+=f'{titulo},{categoria},{link}\n'
-        except:
-          ...
-    url="headlines/final/periodico=ElEspectador/year="+str(fecha.year)+"/month="+str(fecha.strftime('%m'))+"/elEspectador"
+          texto += get_link_ee(links)
+        
+    url="headlines/final/periodico=ElEspectador/year="+str(fecha.year)+"/month="+str(fecha.strftime('%m'))+"/day="+str(fecha.strftime('%d'))+"/elEspectador"
     s3_resource.Object(S3_BUCKET, url+'{}.csv'.format(fecha.strftime('%Y-%m-%d'))).put(Body=texto)
 
 extraerTiempo("headlines/raw/ElTiempo-"+str(fecha.year)+"-"+str(fecha.strftime('%m'))+"-"+str(fecha.strftime('%d'))+".html")
